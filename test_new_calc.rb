e4b90f94033afaecafce8c057c3ac53001845166
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-09 18:20:02 -0400

    10th test that passed, test_if_string_equals_nil1_subtraction

--- test_new_calc.rb
+++ test_new_calc.rb
@@ -53,6 +53,6 @@ class TestNewCalc < Minitest::Test
 	end
 
 	def test_if_string_entered_equals_nil1_subtraction
-		assert_equal(19, subtraction("", 67))
+		assert_equal(nil, subtraction("", 67))
 	end
 end
