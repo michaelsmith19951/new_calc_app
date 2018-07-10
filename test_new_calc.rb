108f154e7a693e5adf5b08feeedd7d678053e0b6
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-10 00:03:44 -0400

    27th test that failed, test_if_string_equals_nil2_division

--- test_new_calc.rb
+++ test_new_calc.rb
@@ -87,4 +87,8 @@ class TestNewCalc < Minitest::Test
 	def test_if_string_entered_equals_nil1_division
 		assert_equal(nil, division("", 10))
 	end
+
+	def test_if_string_entered_equals_nil2_division
+		assert_equal(34, division(78, ""))
+	end
 end
